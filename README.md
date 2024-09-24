675b24a030e913dae879bfe48edcd8fc55ecf183
andrey <ajivsov@nvidia.com>
2024-09-23 23:41:59 -0700

    Added update feature

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ The tool `xor` takes two input files as input, and creates a third file, whose c
 
     $ xor -a file1.txt -b file2.txt -o output.bin
 
-Such an operation is reversible. 
+Such an operation is reversible.
 
 ## Example 2
 
-Using files from the above example, 
-    
+Using files from the above example,
+
     $ xor -a file1.txt -b output.bin -o output2
     $ diff output2 file2.txt && echo The same
     The same
@@ -24,11 +24,34 @@ Using files from the above example,
 
 The tool sets the output size equal to the size of the largest file.
 
-This means that when the input is a special file of unlimited size, such as `/dev/urandom`, the tool will never stop.  
+This means that when the input is a special file of unlimited size, such as `/dev/urandom`, the tool will never stop.
 
 ## Example 3
 
 The following command creates the file `unlimited.bin`, whose contents are `file1.txt`, and then it keeps adding zeroes after that. This invocation never completes (because the end-of-file condition cannot be reached for `/dev/zero`).
 
-    $ xor -a file1.txt -b /dev/zero -o unlimited.bin   
+    $ xor -a file1.txt -b /dev/zero -o unlimited.bin
+
+## High-performance mode
+
+The tool detects the update scenario and takes advantage of it as follows.
+
+When one of input and output files satisfy the following conditions:
+    * they are regular files
+    * name of one input file matched the output file name (call is file "output" file)
+    * the size of output file is larger than the other file (call it "intput" file)
+then the output file if updated in-place.
+
+When the input file is small, this update is an instant operation. Furthermore, some fileystems such as btrfs perform copy on write, so the following scenario performs instant masking for _arbitrary_ large files:
+
+    $ cp LARGE copy
+    $ xor -a LARGE -b mask -o copy
+    $ diff LARGE copy
+    Binary files LARGE and copy differ
+    $ xor -a copy -b mask -o copy
+    # so far these are instant operations
+
+    $ diff LARGE copy && echo the same
+    the same
+
 
--- xor.c
+++ xor.c
@@ -7,6 +7,26 @@
 #include <sys/stat.h>
 #include <sys/sysmacros.h>
 
+#include <sys/types.h>
+#include <sys/mman.h> /* mmap() is defined in this header */
+#include <fcntl.h>
+
+static unsigned xor_is_regular( int fd ) {
+	struct stat s;
+	int r = fstat(fd, &s);
+	if( r == -1 )
+		return 0;	//false
+	return (s.st_mode & S_IFMT) == S_IFREG;
+}
+
+static size_t xor_file_size( int fd ) {
+	struct stat s;
+	int r = fstat(fd, &s);
+	if( r == -1 )
+		return 0;	//false
+	return s.st_size;
+}
+
 
 static int xor_open( const char *a, const char *b, const char *o, FILE *out[3] )  {
     if ((out[0] = fopen(a, "r")) == NULL) {
@@ -50,6 +70,64 @@ static void xor_buf( uint64_t *a, const uint64_t *b, size_t l) {
 	}
 }
 
+static int xor_mmap_open( const char *sinout, const char *sin, int fd[2] ) {
+	int finout;
+	int fin;
+
+	if ((finout = open (sinout, O_RDWR)) < 0)  {
+		printf("can't open %s for reading and writing", sinout);
+		return 1;
+	}
+	if ((fin = open (sin, O_RDONLY)) < 0)  {
+		printf("can't open %s for reading", sin);
+		close(finout);
+		return 1;
+	}
+	fd[0] = finout;
+	fd[1] = fin;
+	return 0;
+}
+
+static void xor_mmap_close( int fd[2] ) {
+	close(fd[0]);
+	close(fd[1]);
+	fd[0] = fd[1] = -1;
+}
+
+// fd[0] ^= fd[1]
+static int xor_mmap_write( int fd[2] ) {
+	uint8_t *inout;
+	uint8_t *in;
+	size_t l = xor_file_size(fd[1]);
+
+	/* mmap the input file */
+	if ((in = mmap (0, l, PROT_READ, MAP_SHARED, fd[1], 0)) == (uint8_t *) -1) {
+		printf ("mmap error for input");
+		return 1;
+	}
+
+	/* mmap the output file */
+	if ((inout = mmap (0, l, PROT_READ | PROT_WRITE, MAP_SHARED, fd[0], 0)) == (uint8_t *) -1)  {
+		printf ("mmap error for output");
+		return 1;
+	}
+
+	size_t l64 = l - l%64;
+
+	xor_buf( (uint64_t*)inout, (uint64_t*)in, l64 );
+
+	size_t i;
+	for( i=l64; i<l; l++ )  {
+		inout[i] ^= in[i];
+	}
+
+	munmap( inout, l );
+	munmap( in, l );
+
+	return 0;
+}
+
+
 static int xor_write( FILE *file[3] )  {
 #define XOR_BUF_SIZE (1024)
 	uint64_t abuffer[XOR_BUF_SIZE/sizeof(uint64_t)];
@@ -59,8 +137,8 @@ static int xor_write( FILE *file[3] )  {
 	unsigned b_done=0;
 
 	while(1) {
-    	size_t a_read = a_done ? 0 : fread(abuffer, 1, sizeof(abuffer), file[0]);
-    	size_t b_read = b_done ? 0 : fread(bbuffer, 1, sizeof(bbuffer), file[1]);
+		size_t a_read = a_done ? 0 : fread(abuffer, 1, sizeof(abuffer), file[0]);
+		size_t b_read = b_done ? 0 : fread(bbuffer, 1, sizeof(bbuffer), file[1]);
 
 		// Handle short read
 		if( a_read + b_read < sizeof(abuffer) + sizeof(bbuffer) ) {
@@ -73,7 +151,7 @@ static int xor_write( FILE *file[3] )  {
 
 			xor_buf( abuffer, bbuffer, sizeof(abuffer) );
 
-	    	if( fwrite(abuffer, 1, m, file[2]) != m ) {
+			if( fwrite(abuffer, 1, m, file[2]) != m ) {
 				printf("ERROR: Failed to write %jd bytes\n", m );
 				return 1;
 			}
@@ -84,7 +162,7 @@ static int xor_write( FILE *file[3] )  {
 		else {
 			xor_buf( abuffer, bbuffer, sizeof( abuffer ) );
 
-	    	if( fwrite(abuffer, 1, sizeof( abuffer ), file[2]) != sizeof( abuffer )) {
+			if( fwrite(abuffer, 1, sizeof( abuffer ), file[2]) != sizeof( abuffer )) {
 				printf("ERROR: Failed to write %jd bytes\n", sizeof( abuffer ));
 				return 1;
 			}
@@ -101,7 +179,6 @@ main (int argc, char **argv)
   char *bvalue = NULL;
   char *ovalue = NULL;
   int c;
-	FILE *f[3];
 	int err;
 
   opterr = 0;
@@ -140,16 +217,49 @@ main (int argc, char **argv)
 //	for (index = optind; index < argc; index++)
 //		printf ("Non-option argument %s\n", argv[index]);
 
-	err = xor_open( avalue, bvalue, ovalue, f );
-	if( err ) {
-		return err;
+	int fd[2] = {-1,-1};
+
+	/* Special mode: one of input files is the same as output */
+	if( strcmp( avalue, ovalue )==0 ) {
+		err = xor_mmap_open( ovalue, bvalue, fd );
+		if( err )
+			return err;
 	}
-	err = xor_write( f );
-	if( err ) {
+	if( strcmp( bvalue, ovalue )==0 ) {
+		err = xor_mmap_open( ovalue, avalue, fd );
+		if( err )
+			return err;
+	}
+
+	/* Other conditions for mmmap: must be regular files */
+	if( !xor_is_regular(fd[0]) || !xor_is_regular(fd[1]) )
+		xor_mmap_close(fd);
+
+	/* Other conditions for mmmap: first file must be larger */
+	if( xor_file_size(fd[0]) <  xor_file_size(fd[1]) )
+		xor_mmap_close(fd);
+
+	if( fd[0] != -1 )  {
+		// mmap case
+		err = xor_mmap_write(fd);
+		if( err )
+			return err;
+		xor_mmap_close(fd);
+	}
+	else {
+		// general case
+		FILE *f[3];
+		err = xor_open( avalue, bvalue, ovalue, f );
+		if( err ) {
+			return err;
+		}
+		err = xor_write( f );
+		if( err ) {
+			xor_close(f);
+			return err;
+		}
 		xor_close(f);
-		return err;
 	}
-	xor_close(f);
 
 	return 0;
 }
